285b3d298f9c8d17199848891947ad2cd3303ae2
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-05-19 18:03:21 +0530

    M7

---
+++ M7/A2.c
@@ -0,0 +1,63 @@
+// Write versions of the library functions strncpy, strncat, and strncmp; which operate on at most the first n 
+// characters of their argument strings. 
+// strncpy (s, t, n) copies at most n characters of t to s.
+// strncat(s, t, n) appends at most n characters of t to s.
+// strncmp(s, t, n) compares at most n characters of s and t. Returns 0 if at most n characters are equal, 1 otherwise.
+#include <stdio.h>
+
+#define MAX 20
+
+char *my_strncpy(char *s, const char *t, int n);
+char *my_strncat(char *s, const char *t, int n);
+int my_strncmp(const char *s, const char *t, int n);
+
+int main(void)
+{
+    char s[MAX] = "hello";
+    char *t = "he";
+
+    printf("input - 'hello', 'he'\n");
+    /* test my_strncpy */
+    my_strncpy(s, t, 5);
+    printf("my_strncpy - s = \"%s\"\n", s);
+    /* test my_strncat */
+    my_strncat(s, t, 20);
+    printf("my_strncat - s = \"%s\"\n", s);
+    /* test my_strncmp */
+    printf("my_strncmp - returns %d comparing \"%s\" with \"%s\"\n", 
+            my_strncmp(s, t, 4), s, t);
+
+    return 0;
+}
+
+char *my_strncpy(char *s, const char *t, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+        *(s + i) = *(t + i) ? *(t + i) : '\0';
+
+    return s;
+}
+
+char *my_strncat(char *s, const char *t, int n)
+{
+    int i;
+
+    while (*++s)
+        ;
+    for (i = 0; i < n && *(t + i); i++)
+        *(s + i) = *(t + i);
+    *(s + i) = '\0';
+
+    return s;
+}
+
+int my_strncmp(const char *s, const char *t, int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+        if(s[i] != t[i] || s[i] == 0 || t[i] == 0)
+            return 1;
+    return 0;
+}
